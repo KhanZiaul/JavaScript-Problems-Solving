e394b0771514e3768f80fd0123a4eb93bb3f2c6d
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-07-25 23:25:17 +0600

    problem5 done

--- problem3.js
+++ problem3.js
@@ -1,2 +1,17 @@
 // Write a JavaScript program to find the most frequent element in an array and return it. 
 
+function mostFrequentElement(givenArray) {
+    let copyOfArray = givenArray
+    console.log(copyOfArray)
+    let frequentElement = []
+    for (i = 0; i < givenArray.length; i++) {
+        for (j =0; i < givenArray.length; i++) {
+            
+        }
+    }
+
+}
+
+const result = mostFrequentElement([3, 5, 2, 5, 3, 3, 1, 4, 5])
+
+console.log(result)
--- problem5.js
+++ problem5.js
@@ -1,2 +1,24 @@
 // Implement a simple JavaScript calculator. The calculator should take two numbers and an operator (+, -, *, /) as input and return the result of the operation.
 
+function calculator(number1, number2, operator) {
+    switch (operator) {
+        case '+':
+            return number1 + number2;
+        case '-':
+            return number1 - number2;
+        case '*':
+            return number1 * number2;
+        case '/':
+            if (number2 !== 0) {
+                return number1 / number2;
+            } else {
+                return 'Division by zero is not possible';
+            }
+        default:
+            return 'Operator is not valid';
+    }
+}
+
+const result = calculator(9, 8, '*')
+
+console.log(result)
