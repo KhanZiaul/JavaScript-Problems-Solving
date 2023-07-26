f2d0baedc0650a599f7675319ebb7f071935437d
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-07-27 04:02:49 +0600

    validation in problem1 done

--- problem1.js
+++ problem1.js
@@ -1,7 +1,12 @@
 // Create a function that takes a string as input and returns the reversed version of the string without using the built-in reverse() method.
 
-function stringReverse(string) {
-    const givenString = string
+function stringReverse(str) {
+
+    if (typeof str !== "string") {
+        return ("The input must be string")
+    }
+
+    const givenString = str
     const splitString = givenString.split('')
     const reverseSplitString = [];
     for (i = splitString.length - 1; i >= 0; i--) {
