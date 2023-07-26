475f593a18e6ac80bb1c1e3f4f79c1ef2c1b7f5b
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-07-27 03:59:58 +0600

    validation in problem4 done

--- problem4.js
+++ problem4.js
@@ -1,10 +1,14 @@
 // Create a function that takes a sorted array of numbers and a target value as input. The function should find two numbers in the array that add up to the target value. Return an array containing the indices of the two numbers.
 
-function findTwoNumbers(givenSortedArray,targetValue) {
+function findTwoNumbers(givenSortedArray, targetValue) {
+
+    if (!Array.isArray(givenSortedArray) || givenSortedArray.length === 0 || isNaN(targetValue)) {
+        return ("The input must be a non-empty array")
+    }
 
     for (i = 0; i < givenSortedArray.length; i++) {
-        if (givenSortedArray[i] + givenSortedArray[i+1] === targetValue) {
-            return [i,i+1]
+        if (givenSortedArray[i] + givenSortedArray[i + 1] === targetValue) {
+            return [i, i + 1]
             break;
         }
     }
@@ -13,6 +17,6 @@ function findTwoNumbers(givenSortedArray,targetValue) {
 
 }
 
-const result = findTwoNumbers([1, 3, 6, 8, 11, 15],9)
+const result = findTwoNumbers([1, 3, 6, 8, 11, 15], 9)
 
 console.log(result)
