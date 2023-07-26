2ce42ef88f148013f5c04c3399b99a6eb2effdd0
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-07-27 04:21:27 +0600

    validation in problem7 done

--- problem7.js
+++ problem7.js
@@ -1,15 +1,23 @@
 // Implement a JavaScript function to find the second smallest element in an array of numbers. The function should return the second smallest number.
 
 function secondSmallestElement(givenArray) {
-    
-    for (i = 0; i < givenArray.length; i++) {
-        if (givenArray[i] >= 0) {
-            sum= sum + givenArray[i]
+
+    if (!Array.isArray(givenArray) || givenArray.length === 0 || givenArray.length < 2) {
+        return ("The input must be a non-empty array and length must be greater than two")
+    }
+
+    givenArray.sort((a, b) => a - b);
+
+    let secondSmallestElementInArray = [givenArray[0]];
+    for (let i = 1; i < givenArray.length; i++) {
+        if (givenArray[i] !== givenArray[i - 1]) {
+            secondSmallestElementInArray.push(givenArray[i]);
         }
     }
-    return sum;
+
+    return secondSmallestElementInArray[1];
 }
 
-const result = secondSmallestElement([2, 5, 10, 3, 7])
+const result = secondSmallestElement([2, -5, 10, -3, 7])
 
 console.log(result)
