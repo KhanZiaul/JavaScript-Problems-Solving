ab890e912feb554eb33763cd774d3f5b2cf40139
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-07-27 03:57:44 +0600

    validation in problem2 done

--- problem2.js
+++ problem2.js
@@ -1,6 +1,11 @@
 // Create a function that takes an array of numbers as input and returns the sum of all positive numbers in the array. 
 
 function sumOfGivenArray(givenArray) {
+
+    if (!Array.isArray(givenArray) || givenArray.length === 0) {
+        return ("The input must be a non-empty array")
+    }
+
     let sum = 0;
     for (i = 0; i < givenArray.length; i++) {
         if (givenArray[i] >= 0) {
